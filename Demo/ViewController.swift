eda1a7c2a0db3612b1e08d4c572c2e3ac8e681b1
nikhil-manapure <Nikhil.Manapure@ggktech.com>
2017-01-10 10:18:39 +0530

    🚀

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -7,19 +7,79 @@
 //
 
 import UIKit
+import AudioToolbox
+
+enum SoundExtension : String{
+    case caf
+    case aif
+    case wav
+}
+
+let  shortRing = "notification 1"
 
 class ViewController: UIViewController {
-
+    
+    var notificationSoundLookupTable = [String: SystemSoundID]()
+    var shouldPlaySoundEffects = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func play(_ sender: Any) {
+        play(sound: shortRing, ofType: .wav)
+    }
+    
+    func play(sound: String, ofType type: SoundExtension) {
+        if let soundID = notificationSoundLookupTable[sound] {
+            AudioServicesPlaySystemSound(soundID)
+        } else {
+             if let soundURL : CFURL = Bundle.main.url(forResource: sound, withExtension: type.rawValue) as CFURL? {
+                var soundID  : SystemSoundID = 0
+                let osStatus : OSStatus = AudioServicesCreateSystemSoundID(soundURL, &soundID)
+                if osStatus == kAudioServicesNoError {
+                    AudioServicesPlaySystemSound(soundID);
+                    notificationSoundLookupTable[sound] = (soundID)
+                }else{
+                    // This happens in exceptional cases
+                    // Handle it with no sound or retry
+                }
+            }
+        }
+    }
+    
+    func plafy(sound: String, ofType type: SoundExtension) {
+        if let soundID = notificationSoundLookupTable[sound] {
+            AudioServicesPlaySystemSound(soundID)
+        } else {
+            if let soundURL : CFURL = Bundle.main.url(forResource: sound,       withExtension: type.rawValue) as CFURL? {
+                var soundID  : SystemSoundID = 0
+                let osStatus : OSStatus = AudioServicesCreateSystemSoundID(soundURL, &soundID)
+                if osStatus == kAudioServicesNoError {
+                    AudioServicesPlaySystemSound(soundID);
+                    notificationSoundLookupTable[sound] = (soundID)
+                }else{
+                    // This happens in exceptional cases
+                    // Handle it with no sound or retry
+                }
+            }
+        }
+    }
+    
+    func disposeSoundIDs() {
+        for soundID in notificationSoundLookupTable.values {
+            AudioServicesDisposeSystemSoundID(soundID)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.disposeSoundIDs()
     }
 
-
+    deinit {
+        self.disposeSoundIDs()
+    }
 }
+
 
